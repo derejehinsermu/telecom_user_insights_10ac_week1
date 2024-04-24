0318ce67262ca618ed4c460ac7d1ebf7b5e8e659
derejehinsermu <derejehinsermu2@gmail.com>
2024-04-24 21:52:40 +0300

    added new data analysis code

--- src/clean.py
+++ src/clean.py
@@ -1,5 +1,22 @@
 import pandas as pd
 import logging
+import numpy as np
+# Setup logging
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def replace_outliers_with_median(df, column):
+    """Replace outliers in a dataframe column with the column's median."""
+    try:
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+        median = df[column].median()
+        condition = ~df[column].between(Q1 - 1.5 * IQR, Q3 + 1.5 * IQR)
+        df.loc[condition, column] = median
+        # logging.info(f"Outliers replaced in {column}")
+    except KeyError as e:
+        logging.error(f"{column} not found in DataFrame: {e}")
+        raise
 
 def clean_data(df):
     """
@@ -14,15 +31,16 @@ def clean_data(df):
     try:
         # Numerical columns: Replace missing values with the median of each column
         numerical_cols = ['Dur. (ms)', 'Avg RTT DL (ms)', 'Avg RTT UL (ms)',
-                        'Avg Bearer TP DL (kbps)', 'Avg Bearer TP UL (kbps)',
-                        'TCP DL Retrans. Vol (Bytes)', 'TCP UL Retrans. Vol (Bytes)',
-                        'Activity Duration DL (ms)', 'Activity Duration UL (ms)',
-                        'Dur. (ms).1', 'Total UL (Bytes)', 'Total DL (Bytes)']
+                          'Avg Bearer TP DL (kbps)', 'Avg Bearer TP UL (kbps)',
+                          'TCP DL Retrans. Vol (Bytes)', 'TCP UL Retrans. Vol (Bytes)',
+                          'Activity Duration DL (ms)', 'Activity Duration UL (ms)',
+                          'Dur. (ms).1', 'Total UL (Bytes)', 'Total DL (Bytes)']
         
         for col in numerical_cols:
             if col in df.columns:
-                median_value = df[col].median(skipna=True)  # Calculate median ignoring NA
-                df[col] = df[col].fillna(median_value) # Fill NA with median value
+                replace_outliers_with_median(df, col)
+                median_value = df[col].median(skipna=True)
+                df[col] = df[col].fillna(median_value)
 
         # Categorical columns: Replace 'undefined' or missing values with the mode for 'Handset Type'
         if 'Handset Type' in df.columns:
@@ -34,6 +52,12 @@ def clean_data(df):
                     # Replace NaN with the mode for the specific manufacturer
                     df.loc[(df['Handset Manufacturer'] == manufacturer) & (df['Handset Type'].isna()), 'Handset Type'] = manufacturer_mode[0]
         
+        categorical_cols = ['Handset Type', 'Handset Manufacturer', 'Last Location Name']
+        for col in categorical_cols:
+            if col in df.columns:
+                mode_value = df[col].mode()[0]
+                df[col] = df[col].fillna(mode_value)
+                df[col] = df[col].str.title()
         # Normalize string entries to ensure consistent capitalization
         string_cols = ['Handset Manufacturer', 'Handset Type', 'Last Location Name']
         for col in string_cols:
